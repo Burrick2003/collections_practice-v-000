4b85c156e48604a0b4cead9cd6e6a180f36c074c
Blakeley Bond <burrick2003@yahoo.com>
2017-11-02 23:33:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(input_array)#sorts integer array ascending
+end 
 
+def sort_array_desc(input_array)#duh
+end
+
+def sort_array_chair_count(input_array)#sorts ascending by string length
+end
+
+def swap_elements(input_array, index_A, index_B)#swaps 2nd and 3rd elements, input is array, element A, element B
+end
+
+def reverse_array(input_array)#reverses an array
+end
+
+def kesha_maker(input_array)#replaces 3rd character in each string with $
+end
+
+def find_a(input_array)#returns strings that start with "a"
+  
+end
+
+def sum_array(input_array)
+end
+
+def add_s(input_array)#adds a "s" to each word EXCEPT the second element
+end
