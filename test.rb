ffe63a6e6a6be45b69ed301de34f0f2e464d1165
Blakeley Bond <burrick2003@yahoo.com>
2017-11-03 01:37:48 +0000

    automatically backed up by learn

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
-include ./collections_practice.rb#!/usr/bin/env ruby -wKU
-
+#!/usr/bin/env ruby -wKU
+require_relative './collections_practice'
  test_array = ["My", "Halloweeen", "Costume", "Is", "A", "Ocelot."]
  swap_elements_from_to(test_array, 2, 5)
  puts test_array
