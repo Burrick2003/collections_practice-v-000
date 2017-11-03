fd7870b5efcb06c54207527fa4cda40d8c678492
Blakeley Bond <burrick2003@yahoo.com>
2017-11-03 00:49:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,7 @@ def sort_array_desc(input_array)#duh
 end
 
 def sort_array_chair_count(input_array)#sorts ascending by string length
-  input_array.sort_by! do |a|, a.length
+  input_array.sort_by do |a|, a.length
 
   end
 end
