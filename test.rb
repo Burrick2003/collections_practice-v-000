d1fe82be2a6cb5761312ec9a8f6906fe8dc2da3e
Blakeley Bond <burrick2003@yahoo.com>
2017-11-03 01:18:51 +0000

    automatically backed up by learn

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 include ./collections_practice.rb#!/usr/bin/env ruby -wKU
 
  test_array = ["My", "Halloweeen", "Costume", "Is", "A", "Ocelot."]
- swap_elements_from_to(test_array, 2, 5]
+ swap_elements_from_to(test_array, 2, 5)
  puts test_array
